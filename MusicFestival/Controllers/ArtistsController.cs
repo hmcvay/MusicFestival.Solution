dbb19ab6dc75facd211e481a4e9ba1edd98df5cc
hmcvay <hayleymcvay8@gmail.com>
2022-03-16 12:05:11 -0700

    Add artist index to display all artists and action link to create view.

---
+++ MusicFestival/Controllers/ArtistsController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using MusicFestival.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MusicFestival.Controllers
+{
+  public class ArtistsController : Controller
+  {
+    private readonly MusicFestivalContext _db;
+    public ArtistsController(MusicFestivalContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Artist> model = _db.Artists.ToList();
+      return View(model);
+    }
+  }
+}

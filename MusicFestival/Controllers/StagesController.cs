37aee0db069684e411aa1bbf19f1ef4394b00952
hmcvay <hayleymcvay8@gmail.com>
2022-03-17 11:14:13 -0700

    Add delete route and view to stage.
    Co-authored-by: William Jameson <wcjameson@users.noreply.github.com>

--- MusicFestival/Controllers/StagesController.cs
+++ MusicFestival/Controllers/StagesController.cs
@@ -9,51 +9,67 @@ namespace MusicFestival.Controllers
   public class StagesController : Controller
   {
     private readonly MusicFestivalContext _db;
+
     public StagesController(MusicFestivalContext db)
     {
-      _db = db;
+        _db = db;
     }
 
     public ActionResult Index()
     {
-      List<Stage> model = _db.Stages.ToList();
-      return View(model);
+        List<Stage> model = _db.Stages.ToList();
+        return View(model);
     }
+
     public ActionResult Create()
     {
-      return View();
+        return View();
     }
-    
+
     [HttpPost]
     public ActionResult Create(Stage stage)
     {
-      _db.Stages.Add(stage);
-      _db.SaveChanges();
-      return RedirectToAction("Index");
+        _db.Stages.Add(stage);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
     }
 
     public ActionResult Details(int id)
     {
-      var thisStage = _db.Stages
-        .Include(stage => stage.JoinEntities)
-        .ThenInclude(join => join.Artist)
-        .FirstOrDefault(stage => stage.StageId == id);
-      return View(thisStage);
+        var thisStage = _db.Stages
+            .Include(stage => stage.JoinEntities)
+            .ThenInclude(join => join.Artist)
+            .FirstOrDefault(stage => stage.StageId == id);
+        return View(thisStage);
     }
 
     public ActionResult Edit(int id)
     {
-      var thisStage = _db.Stages.FirstOrDefault(stage => stage.StageId == id);
-      return View(thisStage);
+        var thisStage = _db.Stages.FirstOrDefault(stage => stage.StageId == id);
+        return View(thisStage);
     }
-    
+
     [HttpPost]
     public ActionResult Edit(Stage stage)
     {
-      _db.Entry(stage).State = EntityState.Modified;
-      _db.SaveChanges();
-      return RedirectToAction("Index");
+        _db.Entry(stage).State = EntityState.Modified;
+        _db.SaveChanges();
+        return RedirectToAction("Index");
     }
-    
+
+    public ActionResult Delete(int id)
+    {
+        var thisStage = _db.Stages.FirstOrDefault(stage => stage.StageId == id);
+        return View(thisStage);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+        var thisStage = _db.Stages.FirstOrDefault(stages => stages.StageId == id);
+        _db.Stages.Remove(thisStage);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
   }
 }
